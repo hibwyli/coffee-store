46db7f2304cbb2ba1e0ee30d79b81d4602787e0f
minhhieu215 <nguyenhieuyasuo12345@gmail.com>
2025-11-10 12:47:13 +0700

    Test confilg

---
+++ coffee_managements/SessionVars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoAnLapTrinhMang.Models; 
+
+namespace DoAnLapTrinhMang
+{
+    public static class SessionVars
+    {
+        public static UserData AppName = null; 
+    }
+}
