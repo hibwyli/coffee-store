59f64612501240855c162880bb342ae2f0e73984
minhhieu215 <nguyenhieuyasuo12345@gmail.com>
2025-11-24 15:18:43 +0700

    Chon do uong + tinh toan tong tien

--- CoffeeServer/CoffeeServer/Models/DoUong.cs
+++ CoffeeServer/CoffeeServer/Models/DoUong.cs
@@ -15,7 +15,7 @@ namespace CoffeeServer.Models
         public string MaLoai { get; set; }
 
         [FirestoreProperty]
-        public double DonGia { get; set; }
+        public double DonGia { get; set; }  
 
         [FirestoreProperty]
         public string HinhAnh { get; set; }
