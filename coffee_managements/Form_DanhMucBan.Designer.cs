bd9aaa45ccf34fd13fb2496b04d6554a975a67ad
dangvanhoang1711 <dangvanhoang1711@gmail.com>
2025-12-09 17:07:57 +0700

    haha

---
+++ coffee_managements/Form_DanhMucBan.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DoAnLapTrinhMang
+{
+    partial class Form_DanhMucBan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_DanhMucBan));
+            this.txtMaLoai = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView_DoUong = new System.Windows.Forms.DataGridView();
+            this.txtTenDU = new System.Windows.Forms.TextBox();
+            this.txtMaDU = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuThem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSua = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuXoa = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuXoaTrang = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuThoat = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DoUong)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMaLoai
+            // 
+            this.txtMaLoai.Location = new System.Drawing.Point(205, 182);
+            this.txtMaLoai.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMaLoai.Name = "txtMaLoai";
+            this.txtMaLoai.Size = new System.Drawing.Size(208, 26);
+            this.txtMaLoai.TabIndex = 58;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 25);
+            this.label3.TabIndex = 52;
+            this.label3.Text = "Số người";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(673, 653);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 40);
+            this.button1.TabIndex = 50;
+            this.button1.Text = "Tìm kiếm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtTim
+            // 
+            this.txtTim.Location = new System.Drawing.Point(221, 661);
+            this.txtTim.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(348, 26);
+            this.txtTim.TabIndex = 49;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 662);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(182, 25);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Tìm kiếm theo tên";
+            // 
+            // dataGridView_DoUong
+            // 
+            this.dataGridView_DoUong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_DoUong.Location = new System.Drawing.Point(0, 242);
+            this.dataGridView_DoUong.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView_DoUong.Name = "dataGridView_DoUong";
+            this.dataGridView_DoUong.RowHeadersWidth = 51;
+            this.dataGridView_DoUong.RowTemplate.Height = 24;
+            this.dataGridView_DoUong.Size = new System.Drawing.Size(900, 382);
+            this.dataGridView_DoUong.TabIndex = 47;
+            // 
+            // txtTenDU
+            // 
+            this.txtTenDU.Location = new System.Drawing.Point(205, 129);
+            this.txtTenDU.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTenDU.Name = "txtTenDU";
+            this.txtTenDU.Size = new System.Drawing.Size(208, 26);
+            this.txtTenDU.TabIndex = 46;
+            // 
+            // txtMaDU
+            // 
+            this.txtMaDU.Location = new System.Drawing.Point(205, 74);
+            this.txtMaDU.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMaDU.Name = "txtMaDU";
+            this.txtMaDU.Size = new System.Drawing.Size(208, 26);
+            this.txtMaDU.TabIndex = 45;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Số bàn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Mã bàn";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuThem,
+            this.menuSua,
+            this.menuXoa,
+            this.menuXoaTrang,
+            this.menuThoat});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(902, 33);
+            this.menuStrip1.TabIndex = 51;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuThem
+            // 
+            this.menuThem.Image = ((System.Drawing.Image)(resources.GetObject("menuThem.Image")));
+            this.menuThem.Name = "menuThem";
+            this.menuThem.Size = new System.Drawing.Size(92, 29);
+            this.menuThem.Text = "Thêm";
+            // 
+            // menuSua
+            // 
+            this.menuSua.Image = ((System.Drawing.Image)(resources.GetObject("menuSua.Image")));
+            this.menuSua.Name = "menuSua";
+            this.menuSua.Size = new System.Drawing.Size(83, 29);
+            this.menuSua.Text = "Sửa ";
+            // 
+            // menuXoa
+            // 
+            this.menuXoa.Image = ((System.Drawing.Image)(resources.GetObject("menuXoa.Image")));
+            this.menuXoa.Name = "menuXoa";
+            this.menuXoa.Size = new System.Drawing.Size(79, 29);
+            this.menuXoa.Text = "Xóa";
+            // 
+            // menuXoaTrang
+            // 
+            this.menuXoaTrang.Image = ((System.Drawing.Image)(resources.GetObject("menuXoaTrang.Image")));
+            this.menuXoaTrang.Name = "menuXoaTrang";
+            this.menuXoaTrang.Size = new System.Drawing.Size(126, 29);
+            this.menuXoaTrang.Text = "Xóa trắng";
+            // 
+            // menuThoat
+            // 
+            this.menuThoat.Image = ((System.Drawing.Image)(resources.GetObject("menuThoat.Image")));
+            this.menuThoat.Name = "menuThoat";
+            this.menuThoat.Size = new System.Drawing.Size(93, 29);
+            this.menuThoat.Text = "Thoát";
+            // 
+            // Form_DanhMucBan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(902, 712);
+            this.Controls.Add(this.txtMaLoai);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTim);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView_DoUong);
+            this.Controls.Add(this.txtTenDU);
+            this.Controls.Add(this.txtMaDU);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form_DanhMucBan";
+            this.Text = "Form_DanhMucBan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DoUong)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMaLoai;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView_DoUong;
+        private System.Windows.Forms.TextBox txtTenDU;
+        private System.Windows.Forms.TextBox txtMaDU;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuThem;
+        private System.Windows.Forms.ToolStripMenuItem menuSua;
+        private System.Windows.Forms.ToolStripMenuItem menuXoa;
+        private System.Windows.Forms.ToolStripMenuItem menuXoaTrang;
+        private System.Windows.Forms.ToolStripMenuItem menuThoat;
+    }
+}
